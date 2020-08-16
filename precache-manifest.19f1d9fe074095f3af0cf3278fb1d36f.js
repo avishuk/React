4f067a5f1dc95c87a75e9a8022f2a67a1cfdc7d7
avishuk <avirshuk@gmail.com>
2020-08-17 04:14:01 +0530

    Updates

---
+++ precache-manifest.19f1d9fe074095f3af0cf3278fb1d36f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10097fc2290077b432900759d44b00cc",
+    "url": "/React/confusion/index.html"
+  },
+  {
+    "revision": "8b028031c47f9b66f678",
+    "url": "/React/confusion/static/css/2.a04c74ad.chunk.css"
+  },
+  {
+    "revision": "0a14fc4e9a311575d5c7",
+    "url": "/React/confusion/static/css/main.17322a8a.chunk.css"
+  },
+  {
+    "revision": "8b028031c47f9b66f678",
+    "url": "/React/confusion/static/js/2.6f75abde.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React/confusion/static/js/2.6f75abde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a14fc4e9a311575d5c7",
+    "url": "/React/confusion/static/js/main.3a0ecb8d.chunk.js"
+  },
+  {
+    "revision": "e89aa562eb40bf51cb3a",
+    "url": "/React/confusion/static/js/runtime-main.42b7be2e.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/React/confusion/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/React/confusion/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/React/confusion/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/React/confusion/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/React/confusion/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
